4202a43278cdfae5e1a82b11a328a61ae1a79515
Evgeny Ilin <xzenya@gmail.com>
2021-06-07 16:47:42 +0300

    add favicon

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta name="yandex-verification" content="87086c37a62d6364" />
     <meta name="viewport" content="width=device-width, user-scalable=no">
     <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&family=Roboto&family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet">
 </head>
 <body>
